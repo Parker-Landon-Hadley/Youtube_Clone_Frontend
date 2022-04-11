6a512208bfa9514d54b2f1133c6c3577805fc099
ParkerHadley <parker.l.hadley@gmail.com>
2022-04-11 19:47:01 -0400

    Added youtube API

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import react from "react";
 import axios from "axios";
 
 function App() {
+
 const [allComments, setAllComments] = useState([{}]);
+const KEY = "AIzaSyCWTfthdoWijFdAAnpJbxdVJbXvBVqZirU";
+
 async function getComments(){
   let comment = await axios.get("http://localhost:3011/api/comments");
 
-console.log(comment.data);
-setAllComments(comment.data)
+  console.log(comment.data);
+  setAllComments(comment.data)
 }
-}
-
-export default App;
 
 return (
   <div className="App">
-
+    
   </div>
 )
+}
+
+export default App;
